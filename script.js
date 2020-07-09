bdabc177a1866f2f5bde95475e35b76c1b461da9
Hannah Smith <hannahpsmith1@gmail.com>
2020-07-08 21:32:33 -0700

    added jquery functionality to projects page

--- script.js
+++ script.js
@@ -9,4 +9,242 @@ $(document).ready(function() {
         $(".navbar-menu").toggleClass("is-active");
   
     });
-  });
+    console.log("tada")
+    $("#spaceforce").click(function(){
+        console.log ("yes")
+        $("#sidebar").empty();
+        $("#sidebar").removeClass("is-invisible");
+        $("#sidebar").addClass("is-visible");
+        var article = $("<article>")
+        article.addClass("title is-child box")
+
+
+
+
+        var title= $("<p>")
+        title.text("asdf")
+        title.addClass("title")
+        article.append(title)
+
+        var subtitle= $("<p>")
+        subtitle.text("asdasdfaf")
+        subtitle.addClass("subtitle")
+        article.append(subtitle)
+
+        var content= $("<div>")
+        var par= $("<p>")
+        par.text("lots of things to say")
+        content.addClass("content")
+        content.append(par)
+        article.append(content)
+
+
+        var button = $("<button/>", {
+            text: "view on github",
+            id: 'btn',
+            // href = "https://www.google.com"
+            click: function (){
+                document.location.href='https://www.google.com'
+            }
+        })
+
+        var buttontoo = $("<button/>", {
+            text: "view on github",
+            id: 'btn',
+            // href = "https://www.google.com"
+            click: function (){
+                document.location.href='https://www.google.com'
+            }
+        })
+
+        article.append(buttontoo)
+        article.append(button)
+        $("#sidebar").append(article)
+        
+        
+        
+
+    })
+
+    $("#password").click(function(){
+        console.log ("yes")
+        $("#sidebar").empty();
+        $("#sidebar").removeClass("is-invisible");
+        $("#sidebar").addClass("is-visible");
+        var article = $("<article>")
+        article.addClass("title is-child box notification is-warning")
+
+
+
+
+
+        var title= $("<p>")
+        title.text("asdf")
+        title.addClass("title")
+        article.append(title)
+
+        var subtitle= $("<p>")
+        subtitle.text("asdasdfaf")
+        subtitle.addClass("subtitle")
+        article.append(subtitle)
+
+        var content= $("<div>")
+        var par= $("<p>")
+        par.text("lots of things to say")
+        content.addClass("content")
+        content.append(par)
+        article.append(content)
+
+
+        var button = $("<button/>", {
+            text: "view on github",
+            id: 'btn',
+            // href = "https://www.google.com"
+            click: function (){
+                document.location.href='https://www.google.com'
+            }
+        })
+
+        var buttontoo = $("<button/>", {
+            text: "view on github",
+            id: 'btn',
+            // href = "https://www.google.com"
+            click: function (){
+                document.location.href='https://www.google.com'
+            }
+        })
+
+        article.append(buttontoo)
+        article.append(button)
+        $("#sidebar").append(article)
+        
+        
+        
+
+    })
+
+    $("#scheduler").click(function(){
+        console.log ("yes")
+        $("#sidebar").empty();
+        $("#sidebar").removeClass("is-invisible");
+        $("#sidebar").addClass("is-visible");
+        var article = $("<article>")
+        article.addClass("title is-child box")
+
+
+
+
+        var title= $("<p>")
+        title.text("asdf")
+        title.addClass("title")
+        article.append(title)
+
+        var subtitle= $("<p>")
+        subtitle.text("asdasdfaf")
+        subtitle.addClass("subtitle")
+        article.append(subtitle)
+
+        var content= $("<div>")
+        var par= $("<p>")
+        par.text("lots of things to say")
+        content.addClass("content")
+        content.append(par)
+        article.append(content)
+
+
+        var button = $("<button/>", {
+            text: "view on github",
+            id: 'btn',
+            // href = "https://www.google.com"
+            click: function (){
+                document.location.href='https://www.google.com'
+            }
+        })
+
+        var buttontoo = $("<button/>", {
+            text: "view on github",
+            id: 'btn',
+            // href = "https://www.google.com"
+            click: function (){
+                document.location.href='https://www.google.com'
+            }
+        })
+
+        article.append(buttontoo)
+        article.append(button)
+        $("#sidebar").append(article)
+        
+        
+        
+
+    })
+//   });
+});
+
+
+//   $("#password").click(function(){
+//     console.log ("yes")
+//     $("#sidebar").empty();
+//     $("#sidebar").removeClass("is-invisible");
+//     $("#sidebar").addClass("is-visible");
+//     var article = $("<article>")
+//     article.addClass("title is-child box")
+
+
+
+
+//     var title= $("<p>")
+//     title.text("asdf")
+//     title.addClass("title")
+//     article.append(title)
+
+//     var subtitle= $("<p>")
+//     subtitle.text("asdasdfaf")
+//     subtitle.addClass("subtitle")
+//     article.append(subtitle)
+
+//     var content= $("<div>")
+//     var par= $("<p>")
+//     par.text("lots of things to say")
+//     content.addClass("content")
+//     content.append(par)
+//     article.append(content)
+
+
+//     var button = $("<button/>", {
+//         text: "view on github",
+//         id: 'btn',
+//         // href = "https://www.google.com"
+//         click: function (){
+//             document.location.href='https://www.google.com'
+//         }
+//     })
+
+//     var buttontoo = $("<button/>", {
+//         text: "view on github",
+//         id: 'btn',
+//         // href = "https://www.google.com"
+//         click: function (){
+//             document.location.href='https://www.google.com'
+//         }
+//     })
+
+//     article.append(buttontoo)
+//     article.append(button)
+//     $("#sidebar").append(article)
+    
+    
+    
+
+// })
+// });
+
+//   $("#spaceforce").click(function(){
+//     console.log ("yes")
+//     $("#sidebar").empty();
+//     $("#sidebar").removeClass("is-invisible");
+//     $("#sidebar").addClass("is-visible");
+    
+    
+
+// })
