de209bb5b4594fbafd9962cfc16a55878dafe8f8
Hannah Smith <hannahpsmith1@gmail.com>
2020-07-08 21:56:26 -0700

    added content to projects page

--- script.js
+++ script.js
@@ -10,107 +10,94 @@ $(document).ready(function() {
   
     });
     console.log("tada")
+    // function for spaceforce
     $("#spaceforce").click(function(){
         console.log ("yes")
         $("#sidebar").empty();
         $("#sidebar").removeClass("is-invisible");
         $("#sidebar").addClass("is-visible");
         var article = $("<article>")
-        article.addClass("title is-child box")
+        article.addClass("title is-child box notification is-danger")
 
-
-
-
-        var title= $("<p>")
-        title.text("asdf")
+        var title= $("<h1>")
+        title.text("Spaceforce")
         title.addClass("title")
         article.append(title)
 
-        var subtitle= $("<p>")
-        subtitle.text("asdasdfaf")
+        var subtitle= $("<h2>")
+        subtitle.text("Using the NASA Images API and Wiki API to pull information about Nasa Missions and open source data")
         subtitle.addClass("subtitle")
         article.append(subtitle)
 
-        var content= $("<div>")
-        var par= $("<p>")
-        par.text("lots of things to say")
-        content.addClass("content")
-        content.append(par)
-        article.append(content)
-
+        // var content= $("<div>")
+        // var par= $("<p>")
+        // par.text("Using the NASA Images API and Wiki API to pull information about Nasa Missions and open source data")
+        // content.addClass("content")
+        // content.append(par)
+        // article.append(content)
 
         var button = $("<button/>", {
-            text: "view on github",
+            text: "Deployed Link",
             id: 'btn',
-            // href = "https://www.google.com"
             click: function (){
-                document.location.href='https://www.google.com'
+                document.location.href='https://hannahpsmith1.github.io/project-one/'
             }
         })
 
         var buttontoo = $("<button/>", {
-            text: "view on github",
+            text: "View on Github",
             id: 'btn',
-            // href = "https://www.google.com"
             click: function (){
-                document.location.href='https://www.google.com'
+                document.location.href='https://github.com/hannahpsmith1/project-one'
             }
         })
 
         article.append(buttontoo)
         article.append(button)
         $("#sidebar").append(article)
-        
-        
-        
+
 
     })
-
+// function for password
     $("#password").click(function(){
         console.log ("yes")
         $("#sidebar").empty();
         $("#sidebar").removeClass("is-invisible");
         $("#sidebar").addClass("is-visible");
         var article = $("<article>")
-        article.addClass("title is-child box notification is-warning")
+        article.addClass("title is-child box notification is-danger")
 
-
-
-
-
-        var title= $("<p>")
-        title.text("asdf")
+        var title= $("<h1>")
+        title.text("Password Generator")
         title.addClass("title")
         article.append(title)
 
-        var subtitle= $("<p>")
-        subtitle.text("asdasdfaf")
+        var subtitle= $("<h2>")
+        subtitle.text("Password Generator that checks for password length of 8 characters or greater with options to have Captial Letters, lowercase letters, numbers and/or special characters ")
         subtitle.addClass("subtitle")
         article.append(subtitle)
 
-        var content= $("<div>")
-        var par= $("<p>")
-        par.text("lots of things to say")
-        content.addClass("content")
-        content.append(par)
-        article.append(content)
+        // var content= $("<div>")
+        // var par= $("<p>")
+        // par.text("lots of things to say")
+        // content.addClass("content")
+        // content.append(par)
+        // article.append(content)
 
 
         var button = $("<button/>", {
-            text: "view on github",
+            text: "Deployed Link",
             id: 'btn',
-            // href = "https://www.google.com"
             click: function (){
-                document.location.href='https://www.google.com'
+                document.location.href='https://hannahpsmith1.github.io/password-generator/'
             }
         })
 
         var buttontoo = $("<button/>", {
-            text: "view on github",
+            text: "View on Github",
             id: 'btn',
-            // href = "https://www.google.com"
             click: function (){
-                document.location.href='https://www.google.com'
+                document.location.href='https://github.com/hannahpsmith1/password-generator'
             }
         })
 
@@ -129,122 +116,49 @@ $(document).ready(function() {
         $("#sidebar").removeClass("is-invisible");
         $("#sidebar").addClass("is-visible");
         var article = $("<article>")
-        article.addClass("title is-child box")
+        article.addClass("title is-child box notification is-danger")
 
 
 
 
-        var title= $("<p>")
-        title.text("asdf")
+        var title= $("<h1>")
+        title.text("Workday Scheduler")
         title.addClass("title")
         article.append(title)
 
-        var subtitle= $("<p>")
-        subtitle.text("asdasdfaf")
+        var subtitle= $("<h2>")
+        subtitle.text("Workday Scheduler with local storage to store typed information by the hour.  Highlights future (green), present (red), and past (grey) hours.")
         subtitle.addClass("subtitle")
         article.append(subtitle)
 
-        var content= $("<div>")
-        var par= $("<p>")
-        par.text("lots of things to say")
-        content.addClass("content")
-        content.append(par)
-        article.append(content)
+        // var content= $("<div>")
+        // var par= $("<p>")
+        // par.text("lots of things to say")
+        // content.addClass("content")
+        // content.append(par)
+        // article.append(content)
 
 
         var button = $("<button/>", {
-            text: "view on github",
+            text: "Deployed Link",
             id: 'btn',
-            // href = "https://www.google.com"
             click: function (){
-                document.location.href='https://www.google.com'
+                document.location.href='https://hannahpsmith1.github.io/work-day-scheduler/'
             }
         })
 
         var buttontoo = $("<button/>", {
-            text: "view on github",
+            text: "View on Github",
             id: 'btn',
-            // href = "https://www.google.com"
             click: function (){
-                document.location.href='https://www.google.com'
+                document.location.href='https://github.com/hannahpsmith1/work-day-scheduler'
             }
         })
 
         article.append(buttontoo)
         article.append(button)
         $("#sidebar").append(article)
-        
-        
-        
 
     })
 //   });
 });
-
-
-//   $("#password").click(function(){
-//     console.log ("yes")
-//     $("#sidebar").empty();
-//     $("#sidebar").removeClass("is-invisible");
-//     $("#sidebar").addClass("is-visible");
-//     var article = $("<article>")
-//     article.addClass("title is-child box")
-
-
-
-
-//     var title= $("<p>")
-//     title.text("asdf")
-//     title.addClass("title")
-//     article.append(title)
-
-//     var subtitle= $("<p>")
-//     subtitle.text("asdasdfaf")
-//     subtitle.addClass("subtitle")
-//     article.append(subtitle)
-
-//     var content= $("<div>")
-//     var par= $("<p>")
-//     par.text("lots of things to say")
-//     content.addClass("content")
-//     content.append(par)
-//     article.append(content)
-
-
-//     var button = $("<button/>", {
-//         text: "view on github",
-//         id: 'btn',
-//         // href = "https://www.google.com"
-//         click: function (){
-//             document.location.href='https://www.google.com'
-//         }
-//     })
-
-//     var buttontoo = $("<button/>", {
-//         text: "view on github",
-//         id: 'btn',
-//         // href = "https://www.google.com"
-//         click: function (){
-//             document.location.href='https://www.google.com'
-//         }
-//     })
-
-//     article.append(buttontoo)
-//     article.append(button)
-//     $("#sidebar").append(article)
-    
-    
-    
-
-// })
-// });
-
-//   $("#spaceforce").click(function(){
-//     console.log ("yes")
-//     $("#sidebar").empty();
-//     $("#sidebar").removeClass("is-invisible");
-//     $("#sidebar").addClass("is-visible");
-    
-    
-
-// })
